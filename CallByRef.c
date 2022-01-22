6c8e532bcb46f6a136f74d64e0402276be5c1c0a
ranjithal2000 <98078380+ranjithal2000@users.noreply.github.com>
2022-01-22 18:34:50 +0530

    Add files via upload
    Swap-call by reference

---
+++ CallByRef.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void swap(int*,int*);
+int main()
+{
+    int a,b;
+    printf("enter the value of a and b\n");
+    scanf("%d%d",&a,&b);
+    printf("Before swapping a=%d,b=%d\n",a,b);
+    swap(&a,&b);
+    printf("After swapping a=%d,b=%d",a,b);
+}
+void swap(int *a,int *b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+    printf("after swapping values in function a=%d, b=%d\n",*a,*b);
+}
